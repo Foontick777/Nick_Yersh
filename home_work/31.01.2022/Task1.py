cdb3335dad5774f36daef86bc1d239f958fbaf4d
Yersh_Nikolai <yershn@gmail.com>
2022-02-02 11:54:39 +0300

    init

---
+++ home_work/31.01.2022/Task1.py
@@ -0,0 +1,28 @@
+def sum(dat):
+    x = 0
+    while dat:
+        a = input('enter: ')
+        if a == "exit":
+            return x
+        x = x + int(a)
+        print(x)
+
+
+def mult(data):
+    l = 1
+    while data:
+        b = input('enter: ')
+        if b == "exit":
+            return l
+        l = l * int(b)
+        print(l)
+
+
+def res(type):
+    if type == "+":
+        return sum(type)
+    elif type == '*':
+        return mult(type)
+
+num = input("+/*: ")
+print(res(*num))
