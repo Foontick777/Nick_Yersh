1aeaf1e9b30bdfd9a79c2e35acf9e6df233e455d
Yersh_Nikolai <yershn@gmail.com>
2022-04-15 17:49:10 +0300

    home_work_18_04_2022

---
+++ home_work/18.04.2022/work18_04/app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Student, Book
+
+
+@admin.register(Student)
+class AdminStudent(admin.ModelAdmin):
+    list_display = ("firstname", "lastname", "age")
+
+
+@admin.register(Book)
+class AdminBook(admin.ModelAdmin):
+    list_display = ('title',)
---
+++ home_work/18.04.2022/work18_04/app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Student(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.firstname} {self.lastname} {self.age}'
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    pages = models.PositiveIntegerField()
+    student = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f'{self.title}'
+
